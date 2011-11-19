c06a37c507d98e66e67cdadce06fec409e8aba51
Tom Janssens <Tom@corebvba.be>
2011-11-19 17:27:23 +0100

    + Initial commit

---
+++ MinimalisticCQRS/MiniCQRS.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SignalR.Hubs;
+
+namespace MinimalisticCQRS
+{
+    public interface IChange
+    {
+        void AccountRegistered(string AccountId, string OwnerName,string AccountNumber);
+        void AmountDeposited(string AccountId, decimal Amount);
+        void AmountWithDrawn(string AccountId, decimal Amount);
+    }
+
+    public class CommandHub : Hub,IChange
+    {
+        IChange[] Changers;
+
+        List<string> RegisteredAccountNumbers = new List<string>();
+        Dictionary<string, decimal> AccountBalances = new Dictionary<string, decimal>();
+
+        public CommandHub(IChange Changer)
+        {
+            this.Changers = new IChange[] {this,Changer};
+        }
+
+        // commands
+        public void RegisterAccount(string AccountId, string OwnerName,string AccountNumber)
+        {
+            Guard.Against(RegisteredAccountNumbers.Contains(AccountNumber), "This account number has already been registered");
+            Apply(x=>x.AccountRegistered(AccountId,OwnerName,AccountNumber));
+        }
+
+        public void DepositAmount(string AccountId, decimal Amount)
+        {
+            Guard.Against(!AccountBalances.ContainsKey(AccountId), "You can not deposit into an unregistered account");
+            Guard.Against(Amount < 0, "You can not deposit an amount < 0");
+            Apply(x=>x.AmountDeposited(AccountId,Amount));
+        }
+
+        public void WithdrawAmount(string AccountId, decimal Amount)
+        {
+            Guard.Against(!AccountBalances.ContainsKey(AccountId), "You can not withdraw from an unregistered account");
+            Guard.Against(Amount < 0, "You can not withdraw an amount < 0");
+            Guard.Against(Amount > AccountBalances[AccountId], "You can not withdraw an amount larger then the current balance");
+            Apply(x => x.AmountWithDrawn(AccountId, Amount));
+        }
+
+        // events
+        void IChange.AccountRegistered(string AccountId, string OwnerName, string AccountNumber)
+        {
+            RegisteredAccountNumbers.Add(AccountNumber);
+            AccountBalances.Add(AccountId, 0);
+        }
+
+        void IChange.AmountDeposited(string AccountId, decimal Amount)
+        {
+            AccountBalances[AccountId] += Amount;
+        }
+
+        void IChange.AmountWithDrawn(string AccountId, decimal Amount)
+        {
+            AccountBalances[AccountId] -= Amount;
+        }
+
+        // helpers
+        void Apply(Action<IChange> action)
+        {
+            foreach (var c in Changers) action(c);
+        }
+
+        class Guard
+        {
+            public static void Against(bool assertion, string message)
+            {
+                if (assertion) throw new InvalidOperationException(message);
+            }
+        }
+    }
+
+    public class AccountDetails
+    {
+        public string Id;
+        public string OwnerName;
+        public Decimal Balance;
+        public string AccountNumber;
+        public string Description { get { return string.Format("{0} - {1}", AccountNumber, OwnerName); } }
+    }
+
+    public class AccountBalances : Dictionary<string, decimal> { }
+
+    public class QueryHub : Hub,IChange
+    {
+        Dictionary<string, AccountDetails> Details;
+
+        public AccountDetails[] GetDetails()
+        {
+            return Details.Values.ToArray();
+        }
+
+        public QueryHub()
+        {
+            Details = new Dictionary<string, AccountDetails>();
+        }
+
+        void IChange.AccountRegistered(string AccountId, string OwnerName, string AccountNumber)
+        {
+            var detail = new AccountDetails { 
+                Id = AccountId,
+                AccountNumber = AccountNumber,
+                OwnerName = OwnerName,
+                Balance = 0
+            };
+            Details.Add(AccountId, detail);
+            Clients.AddAccountDetails(detail);
+        }
+
+        void IChange.AmountDeposited(string AccountId, decimal Amount)
+        {
+            Details[AccountId].Balance += Amount;
+            Clients.UpdateBalance(AccountId, Details[AccountId].Balance);
+        }
+
+        void IChange.AmountWithDrawn(string AccountId, decimal Amount)
+        {
+            Details[AccountId].Balance -= Amount;
+            Clients.UpdateBalance(AccountId, Details[AccountId].Balance);
+        }
+    }
+}
